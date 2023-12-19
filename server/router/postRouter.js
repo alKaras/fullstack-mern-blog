e6674acbd6066765a28ebfc3b0d8ba4b3a97d563
Alexander <al.karas.pr@gmail.com>
2023-12-19 22:51:28 +0200

    delete unnecessary functions with post tags

--- server/router/postRouter.js
+++ server/router/postRouter.js
@@ -4,14 +4,12 @@ const { PostController } = require('../controllers');
 const auth = require('../middleware/auth');
 
 router.get('/getPosts', PostController.getAll);
-router.get('/getPostTags', PostController.getLastTags);
 router.get('/getPost/:id', PostController.getOne);
 router.post('/createPost', auth, PostController.createPost);
 router.patch('/updatePost/:id', auth, PostController.updatePost);
 router.delete('/removePost/:id', auth, PostController.removePost);
 router.get('/getUserPost', auth, PostController.getMyPosts);
 router.get('/search', PostController.getPostsByTags);
-router.get('/getTags', PostController.getLastTags);
 router.get('/getPopularTags', async (req, res) => {
     try {
         const popularTags = await PostController.getPopularTags();
