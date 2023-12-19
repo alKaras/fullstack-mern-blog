146730cca06f169a100983623a0a25118684db1f
Alexander <al.karas.pr@gmail.com>
2023-12-19 22:31:36 +0200

    delete comments

--- client/src/Pages/SignIn/index.jsx
+++ client/src/Pages/SignIn/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-// import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import SignInStyles from './SignIn.module.scss'
